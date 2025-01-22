d82b90049dd30273b10dd584a9b139fa6e53f416
dsppr <by.dsppr@gmail.com>
2025-01-22 20:54:12 +0700

    Set-up pubspec.yaml, import assets, dan membuat mock up data

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:coffee_talk_companion/main.dart';
+import 'package:coffee_talk_companion_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
